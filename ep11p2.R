d74179dd9cce9312b5dac42e50bc8ca731d62b06
sofia <sofia.castro@usach.cl>
2022-01-03 01:10:53 -0300

    Se unen las preguntas 1 y 2

--- ep11p2.R
+++ ep11p2.R
@@ -1,13 +1,73 @@
+#Integrantes
+#Nombre: Sofia Castro; RUT: 20.055.286-5
+#Nombre: Felipe Cornejo; RUT: 20.427.782-6
+#Nombre: Gianfranco Piccinini; Rut: 20.237.081-0
 library(dplyr)
-library ( tidyverse )
-library ( ggpubr )
+library(tidyverse)
+library(ggpubr)
 library(ggplot2)
 library(readxl)
 library(boot)
 install.packages('simpleboot')
 library(simpleboot)
+datosI <- readxl::read_xls("/Users/macbookair/Downloads/Datos-Casen-v2.xls")
+datosS <- datosI %>% select(esc,s4)
+datosS12 <-datosS %>% filter(esc==12)
+datosS12 <-datosS12 %>% select(s4)
+datosS18 <- datosS %>% filter(esc==18)
+datosS18 <- datosS18 %>% select(s4)
 
-datos <- read_xls(choose.files())
+#El nivel de escolaridad de las personas, o sea la cantidad de aÃ±os de estudios
+#se relaciona con la cantidad de hijos que ha tenido. 
+#Verificar que el que posee 12 aÃ±os de estudios posee mayor cantidad de hijos que 
+#una persona con 18 aÃ±os de estudios.
+#Se estipula un alfa de 0.05
+#H0: No hay diferencia de medias entre las muestras.
+#u12-u18=0
+#HA: Existe una diferencia entre las muestras, donde la muestra con 12 aÃ±os posee
+#mayor cantidad de hijos que 18 aÃ±os de escolaridad.
+#u12-u18>0
+datosS12[is.na(datosS12) ]<- 0
+datosS18[is.na(datosS18) ]<- 0
+set.seed(310)
+R<-1000
+n1<-sample (c(250:500), size=1, replace =F)
+n2<-sample (c(250:500), size=1, replace =F)
+muestra1 <- as.numeric(sample(datosS12[["s4"]],size=n1))
+muestra1[is.na(muestra1) ]<- 0
+muestra2 <- sample(datosS18[["s4"]],size=n2)
+observado <-  mean(muestra1)-mean(muestra2)
+distribucion <- c(1:R)
+for (i in 1: R ) {
+  n_1 <-length( muestra1)
+  n_2 <-length( muestra2)
+  permutacion <- sample(c( muestra1, muestra2) , size = n_1 + n_2,
+                        replace = FALSE )
+  permutacion1 <- permutacion [1 : n_1]
+  permutacion2 <- permutacion [n_1 + 1 : n_2]
+  distribucion[i] <- mean(permutacion1)-mean(permutacion2)
+}
+
+numerador <- sum( distribucion> observado ) + 1
+denominador <- R + 1
+valorp <- numerador / denominador
+
+cat (" Valor p:", valorp, "\n\n")
+observaciones <- data.frame( distribucion )
+histograma <- gghistogram(observaciones,x="distribucion",
+                          xlab = "Media",
+                          ylab = " Frecuencia ",bins = 30)
+
+qq <- ggqqplot (observaciones,x ="distribucion")
+
+print(qq)
+print(histograma)
+#En los graficos se puede observar una distribucion normal en las permutaciones generadas.
+#Dado que el  p obtenido es menor al alfa estipulado, se rechaza la hipotesis nula
+#en favor de la hipotesis alternativa, por lo que las personas con 12 aÃ±os de escolaridad
+#poseen mayor cantidad de hijos que los de 18 aÃ±os de escolaridad.
+
+datos <- readxl::read_xls("/Users/macbookair/Downloads/Datos-Casen-v2.xls")
 
 #PREGUNTA 2: Bootstrapping
 #El ingreso per capita es similar en hombres solteros de la region metropolitana
@@ -24,16 +84,16 @@ set.seed(523)
 
 datos2 <- select(datos,region,sexo,ecivil,ytotcorh,numper)
 
-hs_metropolitana <- filter(datos2, region == "Región Metropolitana de Santiago", sexo == "Hombre", ecivil == "Soltero(a)")
+hs_metropolitana <- filter(datos2, region == "Regiï¿½n Metropolitana de Santiago", sexo == "Hombre", ecivil == "Soltero(a)")
 hs_metropolitana <- mutate(hs_metropolitana, pib = ytotcorh / as.integer(numper)) %>% na.omit(hs_metropolitana)
 
-hs_valparaiso <- filter(datos2, region == "Región de Valparaíso", sexo == "Hombre", ecivil == "Soltero(a)")
+hs_valparaiso <- filter(datos2, region == "Regiï¿½n de Valparaï¿½so", sexo == "Hombre", ecivil == "Soltero(a)")
 hs_valparaiso <- mutate(hs_valparaiso, pib = ytotcorh / as.integer(numper))
 
-hs_biobio <- filter(datos2, region == "Región del Biobío", sexo == "Hombre", ecivil == "Soltero(a)")
+hs_biobio <- filter(datos2, region == "Regiï¿½n del Biobï¿½o", sexo == "Hombre", ecivil == "Soltero(a)")
 hs_biobio <- mutate(hs_biobio, pib = ytotcorh / as.integer(numper))
 
-#Muestra de tamaño n = 450
+#Muestra de tamao n = 450
 hs_metropolitana2 <- sample(hs_metropolitana$pib, size = 450)
 hs_valparaiso2 <- sample(hs_valparaiso$pib, size = 450)
 hs_biobio2 <- sample(hs_biobio$pib, size = 450)
@@ -95,17 +155,17 @@ print(qqmv)
 print(qqmb)
 print(qqvb)
 
-cat("Distribución bootstrap entre region m y region v:\n")
+cat("Distribuciï¿½n bootstrap entre region m y region v:\n")
 cat ("\tMedia :", mean(valoresmv$valoresmv,na.rm = TRUE),"\n")
-cat ("\tDesviación estándar:", sd(valoresmv$valoresmv,na.rm = TRUE) , "\n\n")
+cat ("\tDesviaciï¿½n estï¿½ndar:", sd(valoresmv$valoresmv,na.rm = TRUE) , "\n\n")
 
-cat("Distribución bootstrap entre region m y region b:\n")
+cat("Distribuciï¿½n bootstrap entre region m y region b:\n")
 cat ("\tMedia :", mean(valoresmb$valoresmb,na.rm = TRUE),"\n")
-cat ("\tDesviación estándar:", sd(valoresmb$valoresmb,na.rm = TRUE) , "\n\n")
+cat ("\tDesviaciï¿½n estï¿½ndar:", sd(valoresmb$valoresmb,na.rm = TRUE) , "\n\n")
 
-cat("Distribución bootstrap entre region v y region b:\n")
+cat("Distribuciï¿½n bootstrap entre region v y region b:\n")
 cat ("\tMedia :", mean(valoresvb$valoresvb,na.rm = TRUE),"\n")
-cat ("\tDesviación estándar:", sd(valoresvb$valoresvb,na.rm = TRUE) , "\n\n")
+cat ("\tDesviaciï¿½n estï¿½ndar:", sd(valoresvb$valoresvb,na.rm = TRUE) , "\n\n")
 
 #INTERVALOS DE CONFIANZA
 intervalomv <- boot.ci(bootstrapmv, conf = 1-alfa, type = "norm")
@@ -121,3 +181,4 @@ print(intervalovb)
 #-Las medias m y b tienen una distribucion normal
 #-Las medias v y b tienen una distribucion distinta de la normal (inclinada a la derecha)
 #Por lo tanto, las medias se distribuyen de manera diferente entre si, entonces se rechaza H0.
+
